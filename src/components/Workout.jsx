6418cb3a7307868717792083133c1734240bb36b
Rakshit Gupta <rakshitgupta@gmail.com>
2024-09-01 14:31:10 +0530

    first commit

---
+++ src/components/Workout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SectionWrapper from './SectionWrapper'
+import ExerciseCard from './ExerciseCard'
+export default function Workout(props) {
+  const {workout}=props
+  return (
+    <SectionWrapper id={'workout'} header={"welcome to"} title={['The', 'danger', 'zone']}>
+      <div className='flex flex-col gap-4'>
+        {workout.map((exercise,i)=>{
+          return(
+            <ExerciseCard index={i} exercise={exercise} key={i}/>
+          )
+        })}
+      </div>
+    </SectionWrapper>
+  )
+}
